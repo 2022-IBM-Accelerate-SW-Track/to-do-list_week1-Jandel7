44813a23a433d97612538b02306d41e83b4e9cfe
jandel7 <jandel.rodriguez@upr.edu>
2022-06-09 09:17:48 -0400

    Done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_pic from "../assets/profile_pic_name.png";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              className='profile_image'
+              src={profile_pic}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Jessica Wu</div>
+            <div className="brief_description">
+              I am a rising junior at University of Puerto Rico Mayagüez Campus who is majoring in Computer Science an Engineering.
+              A fun fact about me issssss. 
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
