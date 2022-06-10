581b287f9e6e38e290b21fcb8065927062ca9c5c
jandel7 <jandel.rodriguez@upr.edu>
2022-06-10 00:11:47 -0400

    ignore

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic_name.png";
+import profile_pic from "../assets/profile_pic_name.jpg";
 
 export default class About extends Component {
   render() {
@@ -17,11 +17,9 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Jessica Wu</div>
+            <div className="name_title">Jandel Rodríguez Vázquez</div>
             <div className="brief_description">
-              I am a rising junior at University of Puerto Rico Mayagüez Campus who is majoring in Computer Science an Engineering.
-              A fun fact about me issssss. 
-
+              I am a rising junior at University of Puerto Rico who is majoring in Computer Science and Engineering. 
             </div>
           </div>
         </div>
